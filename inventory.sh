9dcad1fc0f2bda3e6cfcd880ba2931bf6e895215
Lars Delhage <lars.delhage@konsult.atg.se>
2022-09-28 21:15:49 +0200

    Fix dynamic inventory script

--- inventory.sh
+++ inventory.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "[k8s]"
-terraform output|sed -e 's/^ip_//' -e 's/ = "/ ansible_host=/' -e 's/"//'
+IP1=$(terraform output -json|jq .ip_cp.value)
+IP2=$(terraform output -json|jq .ip_worker.value)
+echo '{"k8s":{"hosts":["cp","worker"]},"_meta":{"hostvars":{"cp":{"ansible_host":'$IP1'},"worker":{"ansible_host":'$IP2'}}}}'
